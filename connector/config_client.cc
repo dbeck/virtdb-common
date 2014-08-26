89d40f9fb2b0c157493c980e5a22febf7db195de
David Beck <dbeck@starschema.net>
2014-08-26 11:38:38 +0200

    fix compilation issues

--- connector/config_client.cc
+++ connector/config_client.cc
@@ -1,9 +1,12 @@
 #include "config_client.hh"
 
 namespace virtdb { namespace connector {
-
+  
   config_client::config_client(endpoint_client & ep_client)
-  : ep_client_(&ep_client)
+  : ep_client_(&ep_client),
+    zmqctx_(1),
+    cfg_req_socket_(zmqctx_, ZMQ_REQ),
+    cfg_sub_socket_(zmqctx_, ZMQ_SUB)
   {
   }
   
