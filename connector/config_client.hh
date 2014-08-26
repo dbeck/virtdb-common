6cf932f68f9c609065d6ae839f13f9986c7397f2
David Beck <dbeck@starschema.net>
2014-08-26 11:34:16 +0200

    updating proto submodule and minor log_client change

--- connector/config_client.hh
+++ connector/config_client.hh
@@ -7,6 +7,9 @@ namespace virtdb { namespace connector {
   class config_client final
   {
     endpoint_client * ep_client_;
+    zmq::context_t    zmqctx_;
+    zmq::socket_t        ep_req_socket_;
+    zmq::socket_t        ep_sub_socket_;
     
   public:
     config_client(endpoint_client & ep_client);
