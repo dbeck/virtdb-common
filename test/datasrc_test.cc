957537b19935b2f36ad04d94a28f832b73d404e2
David Beck <dbeck@starschema.net>
2014-10-31 00:27:30 +0100

    fix column disposal by using rvalue reference; fix multiple column disposal

--- test/datasrc_test.cc
+++ test/datasrc_test.cc
@@ -16,7 +16,7 @@ TEST_F(PoolTest, Simple)
     c = p.allocate<int32_column>();
     bool res = p.wait_all_disposed(100);
     EXPECT_FALSE(res);
-    c->dispose(c);
+    c->dispose(std::move(c));
     res = p.wait_all_disposed(100);
     EXPECT_TRUE(res);
     
@@ -24,7 +24,7 @@ TEST_F(PoolTest, Simple)
     EXPECT_EQ( p.n_allocated(), 1 );
     EXPECT_EQ( p.n_disposed(), 0 );
     
-    c->dispose(c);
+    c->dispose(std::move(c));
     res = p.wait_all_disposed(1);
     EXPECT_TRUE( res );
     EXPECT_EQ( p.n_allocated(), 1 );
@@ -39,9 +39,9 @@ TEST_F(PoolTest, Async)
   {
     pool p{max_rows};
     c = p.allocate<int32_column>();
-    std::thread t([c]{
+    std::thread t([&c](){
       std::this_thread::sleep_for(std::chrono::milliseconds(100));
-      c->dispose(c);
+      c->dispose(std::move(c));
     });
     t.detach();
     bool res = p.wait_all_disposed(1000);
@@ -54,7 +54,29 @@ TEST_F(PoolTest, AsyncQueued)
   size_t max_rows{10};
   active_queue<column::sptr> q{16,[](column::sptr col){
     column::sptr col_copy = col;
-    col->dispose(col_copy);
+    col->dispose(std::move(col_copy));
+  }};
+  
+  {
+    pool p{max_rows};
+    for( int i=0;i<10000;++i )
+    {
+      column::sptr c = p.allocate<int32_column>();
+      q.push(c);
+    }
+    bool res = p.wait_all_disposed(1000);
+    EXPECT_TRUE(res);
+    std::cout << "allocated:" << p.n_allocated() << "\n";
+  }
+}
+
+TEST_F(PoolTest, AsyncQueuedTripleDispose)
+{
+  size_t max_rows{10};
+  active_queue<column::sptr> q{16,[](column::sptr col){
+    if( col ) col->dispose( std::move(col) );
+    if( col ) col->dispose( std::move(col) );
+    if( col ) col->dispose( std::move(col) );
   }};
   
   {
