928c40e6f6235a191b5deb2ebddadbbce193f040
David Beck <dbeck@starschema.net>
2014-10-31 12:06:40 +0100

    remove unneeded log

--- datasrc/string_column.cc
+++ datasrc/string_column.cc
@@ -1,3 +1,8 @@
+#ifdef RELEASE
+#define LOG_TRACE_IS_ENABLED false
+#define LOG_SCOPED_IS_ENABLED false
+#endif //RELEASE
+
 #include "string_column.hh"
 #include <util/value_type.hh>
 #include <logger.hh>
