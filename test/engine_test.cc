f509b3a307e405a4707f6903fbc795fc12af23fa
David Beck <dbeck@starschema.net>
2014-11-12 11:49:14 +0100

    verify that we receceive the right column on the right subscription

--- test/engine_test.cc
+++ test/engine_test.cc
@@ -1,1 +1,17 @@
 #include "engine_test.hh"
+#include <engine/chunk_store.hh>
+#include <engine/column_chunk.hh>
+#include <engine/data_chunk.hh>
+#include <engine/data_handler.hh>
+#include <engine/expression.hh>
+#include <engine/query.hh>
+#include <engine/receiver_thread.hh>
+#include <engine/util.hh>
+
+using namespace virtdb::util;
+using namespace virtdb::test;
+using namespace virtdb::engine;
+
+
+// check invalid sequence number
+// check invalid query id in the column
