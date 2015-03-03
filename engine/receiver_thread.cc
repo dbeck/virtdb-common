a511a8909e2792eaecf6dc92421eee92b2e2f446
David Beck <dbeck@starschema.net>
2015-03-03 17:17:14 +0100

    fixing memory crash in receiver_thread

--- engine/receiver_thread.cc
+++ engine/receiver_thread.cc
@@ -24,19 +24,12 @@ receiver_thread::receiver_thread()
 
 receiver_thread::~receiver_thread()
 {
-    for (auto it : active_queries)
-    {
-        delete it.second;
-    }
-    active_queries.empty();
 }
 
-void receiver_thread::send_query(
-    push_client<interface::pb::Query>& query_client,
-    sub_client<interface::pb::Column>& data_client,
-    long node,
-    const query& query_data
-)
+void receiver_thread::send_query(push_client<interface::pb::Query>& query_client,
+                                 sub_client<interface::pb::Column>& data_client,
+                                 long node,
+                                 const query& query_data)
 {
     add_query(query_client, data_client, node, query_data);
     query_client.wait_valid();  // TODO : handle timeout here
@@ -44,140 +37,168 @@ void receiver_thread::send_query(
     LOG_INFO("Sent query with id" << V_(query_data.id()) << V_(query_data.table_name()));
 }
 
-void receiver_thread::stop_query(const std::string& table_name, query_push_client& query_client, long node, const std::string& segment_id)
+void
+receiver_thread::stop_query(const std::string& table_name,
+                            query_push_client& query_client,
+                            long node,
+                            const std::string& segment_id)
 {
-    auto * handler = active_queries[node];
-    if (handler != nullptr)
+  auto h = get_data_handler(node);
+  if (h.get())
+  {
+    virtdb::interface::pb::Query stop_query;
+    stop_query.set_queryid(h->query_id());
+    stop_query.set_table(table_name);
+    stop_query.set_querycontrol(virtdb::interface::pb::Query_Command_STOP);
+    stop_query.set_segmentid(segment_id);
+    LOG_TRACE("Sending stop query." <<
+              V_(stop_query.queryid()) <<
+              V_(stop_query.table()) <<
+              V_((int64_t)stop_query.querycontrol()));
+    
+    if( query_client.wait_valid(30000) )
     {
-        virtdb::interface::pb::Query stop_query;
-        stop_query.set_queryid(handler->query_id());
-        stop_query.set_table(table_name);
-        stop_query.set_querycontrol(virtdb::interface::pb::Query_Command_STOP);
-        stop_query.set_segmentid(segment_id);
-        LOG_TRACE("Sending stop query." << V_(stop_query.queryid()) << V_(stop_query.table()) << V_((int64_t)stop_query.querycontrol()));
-        if( query_client.wait_valid(30000) )
-        {
-          query_client.send_request(stop_query);
-        }
-        else
-        {
-          LOG_ERROR("query client is not yet valid" << V_(table_name) << V_(segment_id));
-        }
+      query_client.send_request(stop_query);
     }
-}
-
-
-void receiver_thread::add_query(
-    push_client<interface::pb::Query>& query_client,
-    sub_client<interface::pb::Column>& data_client,
-    long node,
-    const query& query_data
-)
-{
+    else
     {
-    // for (auto i = 0; i < query_data.columns_size(); i++)
-    // {
-        std::string query_id = query_data.id();
-        std::string channel_id = query_id;
-      
-        if (query_data.has_segment_id())
-        {
-            channel_id +=  " " + query_data.segment_id();
-        }
-        data_client.watch(channel_id,
-            [&,query_id,node](const std::string & provider_name,
-                              const std::string & channel,
-                              const std::string & subscription,
-                              std::shared_ptr<interface::pb::Column> column)
-            {
-                if( query_id != column->queryid() )
-                {
-                    LOG_ERROR("inconsistent query id" <<
-                              V_(query_id) <<
-                              V_(column->queryid()) <<
-                              V_(channel) <<
-                              V_(subscription) <<
-                              V_(column->name()) <<
-                              V_(column->seqno()) <<
-                              V_(column->endofdata()) <<
-                              V_((int64_t)node));
-                    return;
-                }
-                else if( subscription.find(column->queryid()) != 0 )
-                {
-                    LOG_ERROR("subscription is inconsistent w/ column's id" <<
-                              V_(query_id) <<
-                              V_(column->queryid()) <<
-                              V_(channel) <<
-                              V_(subscription) <<
-                              V_(column->name()) <<
-                              V_(column->seqno()) <<
-                              V_(column->endofdata()) <<
-                              V_((int64_t)node));
-                    return;
-                }
-              
-                data_handler* handler = get_data_handler(column->queryid());
-                if (handler != nullptr)
-                {
-                    handler->push(column->name(), column);
-                }
-                else
-                {
-                    LOG_ERROR("couldn't find a handler for" <<
-                              V_(column->queryid()) <<
-                              V_(column->name()) <<
-                              V_(provider_name) <<
-                              V_(subscription));
-                }
-            });
-    // }
+      LOG_ERROR("query client is not yet valid" << V_(table_name) << V_(segment_id));
     }
-
-    active_queries[node] =
-      new data_handler(query_data,
-                      [query_data, &query_client](std::string colname, sequence_id_t seqno)
-        {
-            LOG_INFO("Asking for missing chunks." <<
-                     V_(colname) <<
-                     V_(seqno) <<
-                     V_(query_data.segment_id()));
-          
-            virtdb::interface::pb::Query new_query;
-            new_query.set_queryid(query_data.id());
-            new_query.set_table(query_data.table_name());
-            new_query.set_segmentid(query_data.segment_id());
-            auto * field = new_query.add_fields();
-            auto const & tmp_field = query_data.get_field(colname);
-            field->CopyFrom(tmp_field);
-            new_query.add_seqnos(seqno);
-            new_query.set_querycontrol(virtdb::interface::pb::Query_Command_RESEND_CHUNK);
-            query_client.send_request(new_query);
-        }
-    );
+  }
 }
 
-void receiver_thread::remove_query(sub_client<interface::pb::Column>& data_client, long node)
-{
-    data_handler* handler = active_queries[node];
-    data_client.remove_watch(handler->query_id());
-    active_queries.erase(node);
-    delete handler;
-}
 
-data_handler* receiver_thread::get_data_handler(long node)
+void
+receiver_thread::add_query(push_client<interface::pb::Query>& query_client,
+                           sub_client<interface::pb::Column>& data_client,
+                           long node,
+                           const query& query_data)
 {
-    return active_queries[node];
-}
-
-data_handler* receiver_thread::get_data_handler(std::string queryid)
-{
-    for (auto it : active_queries)
+  {
+    std::string query_id = query_data.id();
+    std::string channel_id = query_id;
+    
+    if (query_data.has_segment_id())
     {
-        if (it.second->query_id() == queryid)
-        {
-            return it.second;
-        }
+      channel_id +=  " " + query_data.segment_id();
     }
-    return NULL;
+    data_client.watch(channel_id,
+                      [&,query_id,node](const std::string & provider_name,
+                                        const std::string & channel,
+                                        const std::string & subscription,
+                                        std::shared_ptr<interface::pb::Column> column)
+                      {
+                        if( query_id != column->queryid() )
+                        {
+                          LOG_ERROR("inconsistent query id" <<
+                                    V_(query_id) <<
+                                    V_(column->queryid()) <<
+                                    V_(channel) <<
+                                    V_(subscription) <<
+                                    V_(column->name()) <<
+                                    V_(column->seqno()) <<
+                                    V_(column->endofdata()) <<
+                                    V_((int64_t)node));
+                          return;
+                        }
+                        else if( subscription.find(column->queryid()) != 0 )
+                        {
+                          LOG_ERROR("subscription is inconsistent w/ column's id" <<
+                                    V_(query_id) <<
+                                    V_(column->queryid()) <<
+                                    V_(channel) <<
+                                    V_(subscription) <<
+                                    V_(column->name()) <<
+                                    V_(column->seqno()) <<
+                                    V_(column->endofdata()) <<
+                                    V_((int64_t)node));
+                          return;
+                        }
+                        
+                        auto handler = get_data_handler(column->queryid());
+                        if (handler.get())
+                        {
+                          handler->push(column->name(), column);
+                        }
+                        else
+                        {
+                          LOG_ERROR("couldn't find a handler for" <<
+                                    V_(column->queryid()) <<
+                                    V_(column->name()) <<
+                                    V_(provider_name) <<
+                                    V_(subscription));
+                        }
+                      });
+
+  }
+
+  {
+    lock l(mtx_);
+    active_queries_[node] =
+      handler_sptr(new data_handler(query_data,
+                                    [query_data, &query_client](std::string colname, sequence_id_t seqno)
+                                    {
+                                      LOG_INFO("Asking for missing chunks." <<
+                                               V_(colname) <<
+                                               V_(seqno) <<
+                                               V_(query_data.segment_id()));
+                                      
+                                      virtdb::interface::pb::Query new_query;
+                                      new_query.set_queryid(query_data.id());
+                                      new_query.set_table(query_data.table_name());
+                                      new_query.set_segmentid(query_data.segment_id());
+                                      auto * field = new_query.add_fields();
+                                      auto const & tmp_field = query_data.get_field(colname);
+                                      field->CopyFrom(tmp_field);
+                                      new_query.add_seqnos(seqno);
+                                      new_query.set_querycontrol(virtdb::interface::pb::Query_Command_RESEND_CHUNK);
+                                      query_client.send_request(new_query);
+                                    }
+                                    ));
+  }
+  
+}
+
+void
+receiver_thread::remove_query(sub_client<interface::pb::Column>& data_client,
+                              long node)
+{
+  lock l(mtx_);
+  auto it = active_queries_.find(node);
+  if( it == active_queries_.end() )
+    return;
+  
+  data_client.remove_watch(it->second->query_id());
+  active_queries_.erase(it);
+}
+
+receiver_thread::handler_sptr
+receiver_thread::get_data_handler(long node)
+{
+  handler_sptr ret;
+  {
+    lock l(mtx_);
+    auto it = active_queries_.find(node);
+    if( it != active_queries_.end() )
+      ret = it->second;
+  }
+  return ret;
+}
+
+receiver_thread::handler_sptr
+receiver_thread::get_data_handler(std::string queryid)
+{
+  handler_sptr ret;
+  {
+    lock l(mtx_);
+    for (auto it : active_queries_)
+    {
+      if (it.second->query_id() == queryid)
+      {
+        ret = it.second;
+        return ret;
+      }
+    }
+  }
+  return ret;
 }
--- engine/receiver_thread.hh
+++ engine/receiver_thread.hh
@@ -1,52 +1,61 @@
 // Standard headers
 #include <string>
 #include <map>
+#include <memory>
+#include <mutex>
 
-namespace virtdb {
-  
-    namespace connector
-    {
-        template<typename PUSH_ITEM> class push_client;
-        template<typename SUB_ITEM> class sub_client;
-    }
+namespace virtdb
+{
+  namespace connector
+  {
+    template<typename PUSH_ITEM> class push_client;
+    template<typename SUB_ITEM> class sub_client;
+  }
 }
 
 namespace virtdb {  namespace engine {
+
+  class query;
+  class data_handler;
   
-    class query;
-    class data_handler;
+  class receiver_thread
+  {
+  public:
+    typedef std::shared_ptr<data_handler> handler_sptr;
+    
+  private:
+    typedef virtdb::connector::push_client<virtdb::interface::pb::Query> query_push_client;
+    typedef std::unique_lock<std::mutex> lock;
+    
+    std::map<long, handler_sptr>  active_queries_;
+    std::mutex                    mtx_;
+    
+    void add_query(query_push_client& query_client,
+                   virtdb::connector::sub_client<virtdb::interface::pb::Column>& data_client,
+                   long node,
+                   const virtdb::engine::query& query);
+    
+    receiver_thread& operator=(const receiver_thread&) = delete;
+    receiver_thread(const receiver_thread&) = delete;
+    
+  public:
+    receiver_thread();
+    virtual ~receiver_thread();
+    
+    void remove_query(virtdb::connector::sub_client<virtdb::interface::pb::Column>& data_client, long node);
+    handler_sptr get_data_handler(long node);
+    handler_sptr get_data_handler(std::string queryid);
+    
+    void send_query(query_push_client& query_client,
+                    virtdb::connector::sub_client<virtdb::interface::pb::Column>& data_client,
+                    long node,
+                    const virtdb::engine::query& query);
+    
+    void stop_query(const std::string& table_name,
+                    query_push_client& query_client,
+                    long node,
+                    const std::string& segment_id);
+  };
 
-    class receiver_thread
-    {
-        typedef virtdb::connector::push_client<virtdb::interface::pb::Query> query_push_client;
-
-        private:
-            std::map<long, data_handler* > active_queries;
-
-            void add_query(
-                query_push_client& query_client,
-                virtdb::connector::sub_client<virtdb::interface::pb::Column>& data_client,
-                long node,
-                const virtdb::engine::query& query
-            );
-
-            receiver_thread& operator=(const receiver_thread&) = delete;
-            receiver_thread(const receiver_thread&) = delete;
-
-        public:
-            receiver_thread();
-            virtual ~receiver_thread();
-
-            void remove_query(virtdb::connector::sub_client<virtdb::interface::pb::Column>& data_client, long node);
-            data_handler* get_data_handler(long node);
-            data_handler* get_data_handler(std::string queryid);
-            void send_query(
-                query_push_client& query_client,
-                virtdb::connector::sub_client<virtdb::interface::pb::Column>& data_client,
-                long node,
-                const virtdb::engine::query& query
-            );
-
-            void stop_query(const std::string& table_name, query_push_client& query_client, long node, const std::string& segment_id);
-    };
+  
 }}
