16442a18b9efe39cbcf9cd72deb7101302697068
David Beck <dbeck@starschema.net>
2015-06-04 11:49:56 +0200

    extending monitor client functionality

--- connector/monitoring_client.cc
+++ connector/monitoring_client.cc
@@ -2,6 +2,20 @@
 
 namespace virtdb { namespace connector {
   
+  monitoring_client::sptr monitoring_client::global_instance_;
+  
+  void
+  monitoring_client::set_global_instance(sptr s)
+  {
+    global_instance_ = s;
+  }
+  
+  monitoring_client::sptr
+  monitoring_client::global_instance()
+  {
+    return global_instance_;
+  }
+  
   monitoring_client::monitoring_client(client_context::sptr ctx,
                                        endpoint_client & ep_clnt,
                                        const std::string & server)
@@ -30,7 +44,9 @@ namespace virtdb { namespace connector {
     MonitoringRequest req;
     req.set_type(MonitoringRequest::COMPONENT_ERROR);
     auto inner = req.mutable_comperr();
-    inner->set_type(is_up ? (MonitoringRequest::ComponentError::CLEAR) : (MonitoringRequest::ComponentError::DOWN));
+    inner->set_type(is_up ?
+                    (MonitoringRequest::ComponentError::CLEAR) :
+                    (MonitoringRequest::ComponentError::DOWN));
     inner->set_impactedpeer(name);
     inner->set_reportedby(reported_by);
     
@@ -42,7 +58,8 @@ namespace virtdb { namespace connector {
   
   bool
   monitoring_client::report_state(const std::string & name,
-                                  interface::pb::MonitoringRequest_SetState_Types t)
+                                  interface::pb::MonitoringRequest_SetState_Types t,
+                                  const char * msg)
   {
     using namespace virtdb::interface::pb;
     MonitoringRequest req;
@@ -50,11 +67,37 @@ namespace virtdb { namespace connector {
     auto inner = req.mutable_setst();
     inner->set_name(name);
     inner->set_type(t);
+    if( msg ) inner->set_msg(msg);
     
     return send_request(req,
                         [](const MonitoringReply & rep) { return true; },
                         util::DEFAULT_TIMEOUT_MS);
   }
-
+  
+  bool
+  monitoring_client::report_bad_table_request(const std::string & name,
+                                              interface::pb::MonitoringRequest_RequestError_Types t,
+                                              const std::string & request_id,
+                                              const std::string & table,
+                                              const char * schema,
+                                              const char * msg)
+  {
+    using namespace virtdb::interface::pb;
+    MonitoringRequest req;
+    req.set_type(MonitoringRequest::REQUEST_ERROR);
+    auto inner = req.mutable_reqerr();
+    inner->set_reportedby(name);
+    inner->set_impactedpeer(name);
+    inner->set_type(t);
+    inner->set_requestid(request_id);
+    inner->set_table(table);
+    
+    if( schema )  inner->set_schema(schema);
+    if( msg )     inner->set_message(msg);
+    
+    return send_request(req,
+                        [](const MonitoringReply & rep) { return true; },
+                        util::DEFAULT_TIMEOUT_MS);
+  }
   
 }}
--- connector/monitoring_client.hh
+++ connector/monitoring_client.hh
@@ -10,21 +10,40 @@ namespace virtdb { namespace connector {
     public req_client<interface::pb::MonitoringRequest,
                       interface::pb::MonitoringReply>
   {
-    typedef req_client<interface::pb::MonitoringRequest,
-                       interface::pb::MonitoringReply>     req_base_type;
   public:
     typedef std::shared_ptr<monitoring_client> sptr;
+
+  private:
+    typedef req_client<interface::pb::MonitoringRequest,
+                       interface::pb::MonitoringReply>     req_base_type;
     
+    static sptr global_instance_;
+    
+  public:
     monitoring_client(client_context::sptr ctx,
                       endpoint_client & ep_clnt,
                       const std::string & server);
     
-    bool report_up_down(const std::string & name,
-                        bool is_up,
-                        const std::string & reported_by);
+    bool
+    report_up_down(const std::string & name,
+                   bool is_up,
+                   const std::string & reported_by);
     
-    bool report_state(const std::string & name,
-                      interface::pb::MonitoringRequest_SetState_Types t);
+    bool
+    report_state(const std::string & name,
+                 interface::pb::MonitoringRequest_SetState_Types t,
+                 const char * msg = nullptr);
+    
+    bool
+    report_bad_table_request(const std::string & name,
+                             interface::pb::MonitoringRequest_RequestError_Types t,
+                             const std::string & request_id,
+                             const std::string & table,
+                             const char * schema = nullptr,
+                             const char * msg = nullptr);
+    
+    static void set_global_instance(sptr s);
+    static sptr global_instance();
     
     virtual ~monitoring_client();
     virtual void cleanup();
